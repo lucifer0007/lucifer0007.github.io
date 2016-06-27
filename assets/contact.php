7a75163e767b20c99513d15b309f5bb8ff852650
lucifer0007 <cullen0064@gmail.com>
2016-06-27 15:39:38 +0530

    Contact page change

--- assets/contact.php
+++ assets/contact.php
@@ -1,21 +1,24 @@
 <?php
-
+ 
 // Email address verification
 function isEmail($email) {
-	return filter_var($email, FILTER_VALIDATE_EMAIL);
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
-
+ 
 if($_POST) {
-
     // Enter the email where you want to receive the message
     $emailTo = 'contact.azmind@gmail.com';
-
+ 
+    $name = addslashes(trim($_POST['name']));
     $clientEmail = addslashes(trim($_POST['email']));
     $subject = addslashes(trim($_POST['subject']));
     $message = addslashes(trim($_POST['message']));
-
-    $array = array('emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
-
+ 
+    $array = array('nameMessage' => '', 'emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
+ 
+    if($name == '') {
+        $array['nameMessage'] = 'Empty name!';
+    }
     if(!isEmail($clientEmail)) {
         $array['emailMessage'] = 'Invalid email!';
     }
@@ -25,14 +28,15 @@ if($_POST) {
     if($message == '') {
         $array['messageMessage'] = 'Empty message!';
     }
-    if(isEmail($clientEmail) && $subject != '' && $message != '') {
+    if($name != '' && isEmail($clientEmail) && $subject != '' && $message != '') {
         // Send email
-		$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
-		mail($emailTo, $subject . " (bootstrap contact form)", $message, $headers);
+        $message = "Message from: " . $name . "\r\n" . $message;
+        $headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
+        mail($emailTo, $subject . " (bootstrap contact form)", $message, $headers);
     }
-
+ 
     echo json_encode($array);
-
+ 
 }
-
+ 
 ?>
